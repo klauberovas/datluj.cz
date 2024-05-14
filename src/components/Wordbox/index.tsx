2dafc78a3e9af578bd7ede95edf80c72e38327d6
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2024-05-14 13:14:01 +0200

    Dynamic Letter Removal in Wordbox

--- src/components/Wordbox/index.tsx
+++ src/components/Wordbox/index.tsx
@@ -1,16 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 
 interface IWordboxProp {
   word: string;
 }
 
-const Wordbox : React.FC<IWordboxProp> = ({ word }) => {
-  const [lettersLeft] = useState<string>(word);  
-  
-  return (
-    <div className="wordbox">{lettersLeft}</div>
-  );
+const Wordbox: React.FC<IWordboxProp> = ({ word }) => {
+  const [lettersLeft, setLettersLeft] = useState<string>(word);
+
+  const handleKeyUp = (e: KeyboardEvent) => {
+    console.log('handleKeyUp');
+
+    const firstLetter = lettersLeft.slice(0, 1);
+
+    if (e.key === firstLetter) {
+      setLettersLeft(lettersLeft.slice(1));
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('keyup', handleKeyUp);
+    return () => {
+      document.removeEventListener('keyup', handleKeyUp);
+    };
+  }, [lettersLeft]);
+
+  return <div className="wordbox">{lettersLeft}</div>;
 };
 
 export default Wordbox;
